7779b247c13cfc182002f174a73839a33d9b60e2
Yu Zhang <atomhust@gmail.com>
2014-04-24 10:26:34 -0400

    add friends.

--- src/edu/cmu/smartphone/telemedicine/AddActivity.java
+++ src/edu/cmu/smartphone/telemedicine/AddActivity.java
@@ -3,13 +3,17 @@ package edu.cmu.smartphone.telemedicine;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -39,8 +43,19 @@ public class AddActivity extends Activity {
 	    
 	    // search the cloud database to know if the user exit.
 	    Dao_Sqlite dao = new Dao_Sqlite(AddActivity.this);
-	    dao.searchContactCloud(searchText);
 	    
+	    if (searchText.equals(LoginActivity.getCurrentUserID())) {
+	        Toast toast = Toast.makeText(getApplicationContext(),
+                    "Can not add yourself. Please try again.", Toast.LENGTH_LONG);
+                  toast.setGravity(Gravity.CENTER, 0, 0);
+                  LinearLayout toastView = (LinearLayout) toast.getView();
+                  ImageView imageCodeProject = new ImageView(getApplicationContext());
+                  imageCodeProject.setImageResource(R.drawable.ic_action_accept);
+                  toastView.addView(imageCodeProject, 0);
+                  toast.show();
+	    } else {
+	        dao.searchContactCloud(searchText);
+	    }
 	}
 	
 	@Override
